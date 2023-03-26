a7c5c8c5096337165ebe902f3f4c0b4fbf2e1758
Om-i <115377488+Om-i@users.noreply.github.com>
2023-03-26 22:07:04 +0100

    Refactoring with MySQL LOAD DATA Added comments

--- src/minihw4and5/DBUploader.java
+++ src/minihw4and5/DBUploader.java
@@ -1,3 +1,25 @@
+/*
+ * Rationale:
+ *
+ * While the use of java classes like Scanner/File or BufferedReader/Filereader
+ * seemed to be the most straightforward approach, the time to process all the
+ * instructions, even if in batch, was orders of magnitude inferior to methods
+ * that rely on the database to interact with the file directly.
+ *
+ * Here I used the sql command LOAD DATA LOCAL INFILE to retrieve and upload the
+ * csv entry at the speed allowed by the server, effectively removing the
+ * bottleneck caused by the java runtime.
+ * (the code should run for approximately t00:15 for 300k entries, instead of
+ * t01:20 with other methods, for a 500% performance increase)
+ *
+ * Local data parsing is disabled by default and it must be activated either
+ * from the server configuration file, or trough the command
+ * 'allowLoadLocalInfile' provided by the driver.
+ * (see https://dev.mysql.com/doc/connector-j/8.0/en/connector-j-connp-props-security.html#cj-conn-prop_allowLoadLocalInfile )
+ *
+ * The CSV file contains duplicates (e.g. Invoice=489517, StockCode=21912),
+ * hence I refrained from implementing a duplicate check on upload.
+ */
 package minihw4and5;
 
 import java.sql.Connection;
@@ -7,34 +29,57 @@ import java.sql.Statement;
 
 /**
  *
- * @author Bak <sba22064@student.cct.ie>
+ * @author <@student.cct.ie>
  */
 public class DBUploader {
-    private final String DB_URL = "jdbc:mysql://localhost";
-    private final String PASSWD = "pooa";
+    private final String DB_URL = "jdbc:mysql://localhost?allowLoadLocalInfile=true"; // The url query string referencing the driver setting for local data upload
     private final String USER = "pooa";
+    private final String PASSWD = "pooa";
 
-    public void uploadCSV(String csvPath) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-        Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-        try {
-            Connection conn = DriverManager.getConnection(DB_URL , USER , PASSWD);
-            Statement stmt = conn.createStatement();
-            stmt.addBatch("CREATE DATABASE IF NOT EXISTS minihw4and5;");
-            stmt.addBatch("USE minihw4and5;");
-            stmt.addBatch("CREATE TABLE IF NOT EXISTS newdata_csv ("
-                    + "Invoice int,"
-                    + "StockCode int,"
-                    + "Description varchar(50),"
-                    + "Quantity int,"
-                    + "InvoiceDate datetime,"
-                    + "Price decimal(5,2),"
-                    + "Customer_ID int,Country varchar(20)"
-                    + ");");
-            stmt.executeBatch();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+//    public void query(String sql) throws SQLException{
+//        Connection conn = DriverManager.getConnection(DB_URL , USER , PASSWD);
+//        Statement stmt = conn.createStatement();
+//        stmt.addBatch(sql);
+//        stmt.executeBatch();
+//        conn.close();
+//    }
+    
+    public void loadCSV(String csvPath) throws SQLException {
 
+//        Class.forName("com.mysql.cj.jdbc.Driver").newInstance(); // Deprecated, see https://docs.oracle.com/javase/8/docs/api/java/sql/DriverManager.html
+        Connection conn = DriverManager.getConnection(DB_URL , USER , PASSWD);
+
+        String sql = "LOAD DATA LOCAL INFILE '" + csvPath + "' "
+                     + "INTO TABLE newdata_csv "
+                     + "FIELDS TERMINATED BY ',' " // field separator
+                     + "LINES TERMINATED BY '\\n' " // line separator
+                     + "IGNORE 1 LINES ("
+                     + "Invoice,"
+                     + "StockCode,"
+                     + "Description,"
+                     + "Quantity,"
+                     + "@InvoiceDate,"
+                     + "Price,"
+                     + "Customer_ID,"
+                     + "Country"
+                     + ") SET InvoiceDate = STR_TO_DATE(@InvoiceDate, '%d/%m/%Y %H:%i')";
+
+        Statement stmt = conn.createStatement();
+        stmt.addBatch("CREATE DATABASE IF NOT EXISTS minihw4and5;");
+        stmt.addBatch("USE minihw4and5;");
+        stmt.addBatch("CREATE TABLE IF NOT EXISTS newdata_csv ("
+                      + "Invoice int,"
+                      + "StockCode int,"
+                      + "Description varchar(50),"
+                      + "Quantity int,"
+                      + "InvoiceDate datetime,"
+                      + "Price decimal(5,2),"
+                      + "Customer_ID int,"
+                      + "Country varchar(20)"
+                      + ");");
+        stmt.addBatch(sql);
+        stmt.executeBatch();
+        conn.close();
     }
 
-}// end of class
+}
--- src/minihw4and5/MiniHW4And5.java
+++ src/minihw4and5/MiniHW4And5.java
@@ -1,13 +1,6 @@
 package minihw4and5;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import java.sql.SQLException;
 /**
  *
  * @author <sba22064@student.cct.ie>
@@ -16,25 +9,11 @@ public class MiniHW4And5 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws ClassNotFoundException , InstantiationException , IllegalAccessException {
-//        DBUploader dbup = new DBUploader();
-//        dbup.uploadCSV("");
-        try {
-            System.out.println(parseCSV("src/minihw4and5/newData.csv").get(0).get(0));
-        } catch (IOException e) {
-            System.out.println(e);
-        };
+    public static void main(String[] args) throws SQLException {
+        DBUploader dbup = new DBUploader();
+        dbup.loadCSV("src/minihw4and5/newData.csv");
+
     }//main
 
-    static ArrayList<List<String>> parseCSV(String path) throws IOException , FileNotFoundException {
-        ArrayList<List<String>> table = new ArrayList<>();
-        BufferedReader br = new BufferedReader(new FileReader(path));
-        String tuple;
-        while ((tuple = br.readLine()) != null) {
-            String[] attributes = tuple.split(",");
-            table.add(Arrays.asList(attributes));
-        }
-        return table;
-    }
 
-}//class
+}//class//class
