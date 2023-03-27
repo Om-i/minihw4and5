b5e90b92d7074179b65a4f85c9e78dac353254c4
Om-i <115377488+Om-i@users.noreply.github.com>
2023-03-27 20:25:58 +0100

    Added comments

--- src/minihw4and5/MiniHW4And5.java
+++ src/minihw4and5/MiniHW4And5.java
@@ -36,7 +36,11 @@ import java.util.Scanner;
  * @author <sba22064@student.cct.ie>
  */
 public class MiniHW4And5 {
-    static void runQuery(String url , String userName , String passWord , String sqlPath) throws SQLException , FileNotFoundException {
+    /*
+     * QUERY PARSER
+     * Parses the sql file and sends the command to the database
+     */
+    static void runQuery(String url, String userName, String passWord, String sqlPath) throws SQLException, FileNotFoundException {
         /*
          * Java 6 made this call unnecessary, see
          * https://docs.oracle.com/javase/8/docs/api/java/sql/DriverManager.html
@@ -44,20 +48,27 @@ public class MiniHW4And5 {
 //        Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
 
         /*
-         *
+         * OPEN CONNECTION
          */
-        Connection conn = DriverManager.getConnection(url + "?allowLoadLocalInfile=true" , userName , passWord); // The url query string referencing the driver setting for local data upload
+        Connection conn = DriverManager.getConnection(
+                url + "?allowLoadLocalInfile=true", // The url query string referencing the driver setting for local data upload
+                userName,
+                passWord
+        );
         Statement stmt = conn.createStatement();
         conn.setAutoCommit(false); // prevents database interaction before all the queries are loaded
         /*
-         *
+         * ADD STATEMENTS FROM SQL FILE
+         * Scanner reads the file in sections delimited by a semicolon, the
+         * regex prevents the delimiter removal from the string by moving the
+         * selection cursor right after the symbol. (Positive Lookbehind)
          */
-        Scanner sc = new Scanner(new File(sqlPath)).useDelimiter("(?<=;)"); // Reads the file in sections delimited by a semiclumn, the regex prevents the delimiter removal from the string
+        Scanner sc = new Scanner(new File(sqlPath)).useDelimiter("(?<=;)");
         while (sc.hasNext()) {
-            stmt.addBatch(sc.next()); // Add a single query to the statement batch.
+            stmt.addBatch(sc.next()); // Add every single query to the statement batch in a loop.
         }
         /*
-         *
+         * SEND STATEMENT AND CLOSE
          */
         update(stmt.executeBatch()); // Run the batch and print the number of edits
         conn.commit();               // send the instructions to the database
@@ -65,23 +76,27 @@ public class MiniHW4And5 {
         sc.close();                  // close the scanner
     }
 
+    /*
+     * CONSOLE OUTPUT
+     * Based on the update count returned from the executeBatch() command.
+     */
     static void update(int[] updateCount) {
-        for (int i = 0 ; i < updateCount.length ; i++) {
+        for (int i = 0; i < updateCount.length; i++) {
             System.out.println("Query n. " + (i + 1) + ": " + updateCount[i] + " rows affected.");
         }
     }
 
-    /**
-     * @param args the command line arguments
+    /*
+     * MAIN METHOD
      */
-    public static void main(String[] args) throws SQLException , FileNotFoundException {
+    public static void main(String[] args) throws SQLException, FileNotFoundException {
 
         final String url = "jdbc:mysql://localhost";
         final String username = "pooa";
         final String password = "pooa";
         final String sqlPath = "src/minihw4and5/queries.sql";
 
-        runQuery(url , username , password , sqlPath);
+        runQuery(url, username, password, sqlPath);
 
     }//main
 
